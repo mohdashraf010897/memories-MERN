ae0ad81fb5ff48c2242cd7d126bc8830c57040b6
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-12-31 19:28:45 +0530

    server: add code to start backend server

--- server/index.js
+++ server/index.js
@@ -2,3 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// use mongodb to store data - atlas
+
+const CONNECTION_URL =
+  "mongodb+srv://ashraf-mern:ashraf-mern@vidly.ur5nv.azure.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5001;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((e) => console.log(e.message));
