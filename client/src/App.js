2701e1ffb419bc4b260ba641af7c6506161a2021
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-01-03 15:14:16 +0530

    client: add styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,37 @@
+import React from "react";
+import memories from "./images/memories.png";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from "./components/Posts";
+import Form from "./components/Form";
+
 function App() {
-  return <div className="">hey there</div>;
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 }
 
 export default App;
