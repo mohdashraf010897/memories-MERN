24def01fe629ba6dd0b169276356b8245beadce9
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-01-04 15:48:49 +0530

    add welcome route

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/", (req, res) => {
+  res.send("Greetings from memories API");
+});
 app.use("/posts", postRoutes);
 
 // use mongodb to store data - atlas
