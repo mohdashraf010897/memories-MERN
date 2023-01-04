667f6b38863df5e4a4b08a388f175998146eb275
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-01-04 12:36:23 +0530

    server: add like update logic

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -62,3 +62,26 @@ export const deletePost = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
 };
+export const likePost = async (req, res) => {
+  try {
+    const { id: _id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      return res.status(404).send("No post with that id");
+    }
+
+    const post = await PostMessage.findById(_id);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(
+      _id,
+      {
+        likeCount: post.likeCount + 1,
+      },
+      { new: true }
+    );
+
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
